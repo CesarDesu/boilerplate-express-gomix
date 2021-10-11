777d2ccec3c9ac95a13bda0de8f03c448f380c4e
Cesaru Desu <cesaru.desu@gmail.com>
2021-10-11 16:29:01 -0500

    path send

--- myApp.js
+++ myApp.js
@@ -1,9 +1,11 @@
 var express = require("express");
+const path = require("path");
 var app = express();
 console.log("Hello World");
 
 app.get("/", (req, res) => {
-  res.send("Hello Express");
+  //   res.send("Hello Express");
+  res.sendFile(path.join(__dirname, "/views/index.html"));
 });
 
 module.exports = app;
