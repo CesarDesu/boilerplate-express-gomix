27ce678537d8c8aed40b4b0edac6910b442edc60
Cesaru Desu <cesaru.desu@gmail.com>
2021-10-11 17:13:33 -0500

    bugs

--- myApp.js
+++ myApp.js
@@ -3,11 +3,18 @@ const path = require("path");
 var app = express();
 console.log("Hello World");
 
+// app.use(express.json());
+
 app.use("/public", express.static("public"));
 
 app.get("/", (req, res) => {
   //   res.send("Hello Express");
   res.sendFile(path.join(__dirname, "/views/index.html"));
+});
+app.get("/json", (req, res) => {
+  res.json({
+    message: "Hello json",
+  });
 });
 
 module.exports = app;
