93cf87cf09e07106bc3e2f45de05afcdb8986bfe
Cesaru Desu <cesaru.desu@gmail.com>
2021-10-11 16:54:37 -0500

    public

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ const path = require("path");
 var app = express();
 console.log("Hello World");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   //   res.send("Hello Express");
   res.sendFile(path.join(__dirname, "/views/index.html"));
