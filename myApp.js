7e550df6706518f8d594713086a68183c6f33937
Cesaru Desu <cesaru.desu@gmail.com>
2021-10-11 20:12:50 -0500

    query

--- myApp.js
+++ myApp.js
@@ -44,4 +44,9 @@ app.get("/:word/echo", (req, res) => {
   res.json({ echo: word });
 });
 
+app.get("/name", (req, res) => {
+  const { firstname, lastname } = req.query;
+  res.json({ name: `${firstname} ${lastname}` });
+});
+
 module.exports = app;
